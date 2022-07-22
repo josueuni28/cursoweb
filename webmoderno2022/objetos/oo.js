c825dba5dc6aebd8e9fc5efd01aa389bda12cb62
josueuni28 <josueunisempre@gmail.com>
2022-07-22 13:21:39 -0300

    Inicio em Objetos em JS

---
+++ webmoderno2022/objetos/oo.js
@@ -0,0 +1,22 @@
+// CÓDIGO NÃO EXECUTÁVEL
+
+// Procedural
+processamento(valor1, valor2, valor3) //...
+
+//OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //...
+    }
+}
+
+objeto.processamento() // Foco passou a ser o Objeto
+
+// Princípios importantes:
+// 1. Abstração (Pegar coisas do mundo real e trazer para o objeto de forma simples)
+// 2. Encapsulamento (Manter uma interface simples, porém com os códigos e prodimentos fazendo o trabalho mais complexo, evitando uma dependencia grande)
+// 3. Herança - prototype (Herdar atributos de outros Objetos)
+// 4. Polimorfismo (Um objeto pode variar, uma hora ele é uma coisa outra hora pode ser outra coisa)
