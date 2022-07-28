99bf89be4a7e687ffa60bcff2f0c5caa53aa0879
josueuni28 <josueunisempre@gmail.com>
2022-07-28 15:20:58 -0300

    Novas informações sobre exportação em ModuloA

--- webmoderno2022/Node/moduloA.js
+++ webmoderno2022/Node/moduloA.js
@@ -2,3 +2,11 @@
 this.ola = 'Olá Mundo'
 exports.bemVindo = 'Bem-vindo(a)'
 module.exports.ateLogo = 'Até o próximo exemplo!'
+
+/* Porém você não pode atribuir um novo objeto ao "exports", pois tanto o "exports" quando o "this"
+   apontam parra "module.exports" */
+
+// Isso não será exportado/retornado
+exports = {
+    novo: 'Novo atributo'
+}
