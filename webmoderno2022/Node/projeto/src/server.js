babd98f54cb4bb8e4efa753c075b5a8ae1d5e525
josueuni28 <josueunisempre@gmail.com>
2022-07-29 15:33:26 -0300

    Micro projeto usando o Express

---
+++ webmoderno2022/Node/projeto/src/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const porta = 3003
+
+const app = express()
+
+// O atributo .use() executa em qualquer endereço/requisição
+/*
+app.use((req,res, next)=>{
+    console.log('Eu estou em todos os lugares xD')
+    res.send('Olá eu aqui!')
+})
+*/
+
+// Usando os middlewares, com mais de uma requisição GET
+app.get('/produtos',(req,res, next)=>{
+    console.log('Olá Mundo!')
+    next() // Depois dessa execução ele vai para o próximo get
+})
+
+app.get('/produtos',(req,res, next)=>{
+    res.send({ nome: 'Notebook', preco: 2.350})
+})
+
+
+app.listen(porta, ()=>{
+    console.log(`Servidor rodando em localhost, porta: ${porta}`)
+})
