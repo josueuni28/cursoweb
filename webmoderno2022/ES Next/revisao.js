628e52f7bac1b0e6cea62a22f300c602d0886931
josueuni28 <josueunisempre@gmail.com>
2022-07-30 15:12:49 -0300

    Revisão das novidades do ECMAScript 6

---
+++ webmoderno2022/ES Next/revisao.js
@@ -0,0 +1,57 @@
+/* ----------------------- Let e Const ----------------------- */
+{
+    var a = 'a' // Evitar usar
+    let b = 'b'
+    const c = 'c'
+    //c = 'd' // Não permite
+}
+
+console.log(a)
+//console.log(b, c) // Geram erro por estarem fora do escopo
+
+/* ----------------------- Template string ----------------------- */
+const nome = 'Fulano'
+console.log(`Olá ${nome}!`)
+
+/* ----------------------- Destructuring ----------------------- */
+const [j, o, ...resto] = 'Josué'
+console.log(j, o, resto)
+
+const [x, y] = [1, 2, 3]
+console.log(x, y)
+
+const {nome: novoNome, idade} = {nome: 'Luiza', idade:25}
+/* É possível dar um novo nome para a variável que vai receber o valor do atributo,
+   como no atributo nome, o valor foi atribuido para "novoNome" */
+console.log(novoNome, idade)
+
+/* ----------------------- Arrow Fuction ----------------------- */
+const soma = (a, b) => a + b // Não precisa colocar o return
+const soma2 = (a, b) => {
+    // Você for colocar chaves, precisa especificar o return caso a função retorne algo
+    return a + b
+}
+
+// o "This" em arrow fica preso, nunca muda o contexto em que foi criado em uma função Arrow
+const lexico = () => this === exports
+const l2 = lexico.bind({}) // Mesmo forçando uma alteração do this, ele não muda
+
+console.log(lexico(), l2())
+
+/* ----------------------- Parâmetro default ----------------------- */
+function mostra(nome = 'Dev'){
+    console.log(nome)
+}
+
+mostra('Josué')
+mostra() // imprime Dev
+
+/* ----------------------- Operador rest ----------------------- */
+function somaTodos(...numeros) {
+    /* Os três pontos, indicam que a apartir daquele momento pode ter
+       incontáveis argumentos, e eles serão alocados todos em um array */
+    const total = numeros.reduce((a,b) => a + b)
+    return total
+}
+
+console.log(somaTodos(1,5,9,10,5))
