ee796bea4a5df75fdefabbc8bd564b5f293bb76e
josueuni28 <josueunisempre@gmail.com>
2022-09-02 23:37:44 -0300

    Continuação do Projeto com React

---
+++ webmoderno2022/React/crud/frontend/src/components/templates/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from "./Header"
+import './Main.css'
+
+export default props =>
+    <>
+        <Header />
+        <main className="content">
+            Conteudo
+        </main>
+    </>
