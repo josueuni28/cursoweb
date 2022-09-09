d3c7b9cd67990e747f5de598115e32a2f4f57535
josueuni28 <josueunisempre@gmail.com>
2022-09-08 21:17:14 -0300

    Projeto Next.js CRUD com Firebase,Tailwind e TS

---
+++ webmoderno2022/NextJS/next-crud/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='flex text-4xl h-screen justify-center items-center bg-gradient-to-r from-purple-500 to-blue-400'>
+      New Project Next.js with TS, Firebase and Tailwind
+    </div>
+  )
+}
