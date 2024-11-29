2f8e4d8fc0ec08f50aecc486b073e08819b8b48a
Josué L Souza <josueunisempre@gmail.com>
2024-11-29 19:47:48 -0300

    Continuação do Projeto Angular

--- webmoderno2022/Angular/frontend/src/app/app.component.ts
+++ webmoderno2022/Angular/frontend/src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/template/header/header.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  imports: [RouterOutlet, HeaderComponent, MatToolbarModule],
+  templateUrl: 'app.component.template.html',
 })
 export class AppComponent {
-  title = 'frontend';
+  nome = 'Josué';
 }
